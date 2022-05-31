cf42d072c280ac494915abf0910de5d51e569d5d
Péter Markó <peter.marko1089@gmail.com>
2022-05-31 19:33:48 +0200

    delete unused imports

--- src/main/java/com/WizardsOfTheCoast/magic/controller/CardController.java
+++ src/main/java/com/WizardsOfTheCoast/magic/controller/CardController.java
@@ -2,20 +2,10 @@ package com.WizardsOfTheCoast.magic.controller;
 
 
 import com.WizardsOfTheCoast.magic.service.CardService;
-import com.google.gson.Gson;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 
 @Controller
 public class CardController {
