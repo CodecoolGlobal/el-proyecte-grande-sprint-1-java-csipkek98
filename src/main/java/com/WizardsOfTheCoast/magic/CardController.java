040f9de2edb9cba42831133698fcbcb2ee05b7b0
Péter Markó <peter.marko1089@gmail.com>
2022-05-31 19:07:54 +0200

    add: index.html can show cards picture accordingly after get request

--- src/main/java/com/WizardsOfTheCoast/magic/CardController.java
+++ src/main/java/com/WizardsOfTheCoast/magic/CardController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Controller
@@ -23,8 +25,9 @@ public class CardController {
     }
 
     @GetMapping(value = "/morecards")
-    public Object getAllCards(){
-        String url = "https://api.magicthegathering.io/v1/cards?pageSize=10";
+    public List<String> getAllCards(){
+        ArrayList<String> imageUrls = new ArrayList<String>();
+        String url = "https://api.magicthegathering.io/v1/cards?pageSize=3";
         RestTemplate restTemplate = new RestTemplate();
         Object cards = restTemplate.getForObject(url, Object.class);
         String jsonInString = new Gson().toJson(cards);
@@ -34,12 +37,13 @@ public class CardController {
             Set<String> asd = obj.getJSONObject(i).keySet();
             if(asd.contains("imageUrl")){
                 System.out.println(obj.getJSONObject(i).getString("imageUrl"));
+                imageUrls.add(obj.getJSONObject(i).getString("imageUrl"));
             }else
                 System.out.println("no image");
         }
 
 
-        return restTemplate.getForObject(url, Object.class);
+        return imageUrls;
     }
 
     @GetMapping(value = "/")
