e3031beb53307257fd8db153f8468290928a1cc5
Péter Markó <peter.marko1089@gmail.com>
2022-05-31 21:29:47 +0200

    add JsonArrayConverter method

--- src/main/java/com/WizardsOfTheCoast/magic/service/CardService.java
+++ src/main/java/com/WizardsOfTheCoast/magic/service/CardService.java
@@ -3,6 +3,7 @@ package com.WizardsOfTheCoast.magic.service;
 import com.google.gson.Gson;
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,13 +13,15 @@ import java.util.Set;
 @Service
 public class CardService {
 
+    private JsonArrayConverter converter;
+    @Autowired
+    public void setService(JsonArrayConverter converter) {
+        this.converter = converter;
+    }
+
     public List<String> cardPictures(String pageNumber){
         ArrayList<String> imageUrls = new ArrayList<>();
-        RestTemplate restTemplate = new RestTemplate();
-        Object cards = restTemplate.getForObject(CardEndpoints.CARD_PICTURES.getPath(), Object.class, pageNumber);
-        String jsonInString = new Gson().toJson(cards);
-        JSONObject mJSONObject = new JSONObject(jsonInString);
-        JSONArray obj = mJSONObject.getJSONArray("cards");
+        JSONArray obj = converter.getJSONArray("cards",pageNumber);
         for (int i = 0; i < obj.length(); i++) {
             Set<String> asd = obj.getJSONObject(i).keySet();
             if(asd.contains("imageUrl")){
