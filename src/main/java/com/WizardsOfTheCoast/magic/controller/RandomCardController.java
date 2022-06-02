c2b70463d6ced6b91a2eda12b1903d24ebf554ce
Péter Markó <peter.marko1089@gmail.com>
2022-06-02 11:29:10 +0200

    add:getRandomCard method to the RandomCardController class

--- src/main/java/com/WizardsOfTheCoast/magic/controller/RandomCardController.java
+++ src/main/java/com/WizardsOfTheCoast/magic/controller/RandomCardController.java
@@ -1,4 +1,29 @@
 package com.WizardsOfTheCoast.magic.controller;
 
+import com.WizardsOfTheCoast.magic.service.APIEndpoints;
+import com.WizardsOfTheCoast.magic.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class RandomCardController {
+
+    private CardService cardService;
+
+    @Autowired
+    public void setService(CardService cardService) {
+        this.cardService = cardService;
+    }
+
+    @GetMapping(value = "/randcomcard")
+    public String getRandomCard(Model model){
+        List<String> parameters = new ArrayList<>();
+        model.addAttribute("filteredCards",cardService.getCards(parameters, APIEndpoints.RANDOM));
+        return "carddetails";
+    }
 }
--- src/main/java/com/WizardsOfTheCoast/magic/service/APIEndpoints.java
+++ src/main/java/com/WizardsOfTheCoast/magic/service/APIEndpoints.java
@@ -2,7 +2,8 @@ package com.WizardsOfTheCoast.magic.service;
 
 public enum APIEndpoints {
     FILTER("https://api.scryfall.com/cards/search?q={rarity}={common}"),
-    SEARCH("https://api.scryfall.com/cards/search?q={name}");
+    SEARCH("https://api.scryfall.com/cards/search?q={name}"),
+    RANDOM("https://api.scryfall.com/cards/random");
 
     private String path;
     APIEndpoints(String path){
