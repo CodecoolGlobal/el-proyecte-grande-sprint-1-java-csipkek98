0a18143d957f3bac2ef69697553e005004fd1090
csipkek98 <csipkekrisztian@gmail.com>
2022-06-14 22:15:10 +0200

    Modify: Randomcard.js logic modified for better testing purposes

--- magictrades-frontend/src/components/RandomCard.js
+++ magictrades-frontend/src/components/RandomCard.js
@@ -5,35 +5,44 @@ import axios from 'axios';
 const RandomCards = () => {
     const url = `${process.env.REACT_APP_HOST_URL}/randomcard`;
     const [cardData, setCardData] = useState([]);
-    const fetchCards = () => {
-        axios.get(url).then((response) => {
+    const fetchCards = async () => {
+        await axios.get(url).then((response) => {
             console.log(response);
             const data = response.data;
             setCardData(data);
         });
     }
     useEffect(() => {
-        fetchCards();
-        console.log('i fire once');
+        fetchCards().then(r => console.log('i fire once'));
     },[]);
     return (
-        <div>
-            {Object.values(cardData).map((value, index) => {
-                if(index=== 2){
-                    return (
-                        <div key={index}>
-                        <img
-                            src={value}
-                            alt="new"
-                        />
+        <div id={"card-details"}>
+            {Object.keys(cardData).map((key, index) => {
+                if(cardData[key] !== null){
+                    if(key === "name"){
+                        return <div id={"card-" + key}>
+                            <h1>{cardData[key]}</h1>
                         </div>
-                    )}
-                return (
-                    <div key={index}>
-                        <h2>{value}</h2>
-                        <hr />
-                    </div>
-                );
+                    }else if(key === "id"){
+                        return (
+                            <div id={"card-" + key}>
+                                <p>{cardData[key]}</p>
+                            </div>
+                        )}else if(key === "imageUrl"){
+                        return (
+                            <div id={"card-" + key}>
+                                <img
+                                    src={cardData[key]}
+                                    alt="new"
+                                />
+                            </div>
+                        )}
+                    return (
+                        <div id={"card-" + key}>
+                            <p>{cardData[key]}</p>
+                        </div>
+                    );
+                }
             })}
         </div>
     );
