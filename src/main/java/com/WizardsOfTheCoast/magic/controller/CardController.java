8a7993045485a82d84f225291402b3b55d5b9a9e
Péter Markó <peter.marko1089@gmail.com>
2022-06-14 16:48:24 +0200

    Add: cardController revised version

--- src/main/java/com/WizardsOfTheCoast/magic/controller/CardController.java
+++ src/main/java/com/WizardsOfTheCoast/magic/controller/CardController.java
@@ -1,6 +1,7 @@
 package com.WizardsOfTheCoast.magic.controller;
 
 
+import com.WizardsOfTheCoast.magic.model.CardModel;
 import com.WizardsOfTheCoast.magic.service.APIEndpoints;
 import com.WizardsOfTheCoast.magic.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,12 +25,12 @@ public class CardController {
         this.cardService = cardService;
     }
 
-    @GetMapping(value = "/search/{name}")
-    public String getAllCardsTest(Model model, @PathVariable String name){
+    @GetMapping(value = "/search")
+    @ResponseBody
+    public List<CardModel> getAllCardsTest(@RequestParam(defaultValue="Guest") String name){
         List<String> parameters = new ArrayList<>();
         parameters.add(name);
-        model.addAttribute("cards",cardService.getCards(parameters, APIEndpoints.SEARCH));
-        return "index";
+        return cardService.getCards(parameters, APIEndpoints.SEARCH);
     }
 
 }
