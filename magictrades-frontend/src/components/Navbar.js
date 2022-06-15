d6f3bf86fcfd87810f4f78a4aa048bd753acd40f
csipkek98 <csipkekrisztian@gmail.com>
2022-06-15 13:59:20 +0200

    Add: Different button routing for navbar buttons

--- magictrades-frontend/src/components/Navbar.js
+++ magictrades-frontend/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import {Route, Routes, useNavigate, Link} from "react-router-dom";
 import RandomCard from "./RandomCard";
+import Home from "./Home";
 
 const Navbar = () => {
     const navigate = useNavigate();
@@ -9,14 +10,16 @@ const Navbar = () => {
     return (
             <div>
                 <ul id={"navbar"}>
+                    <li id={"button-home"}><Link to="/">Home</Link></li>
                     <li id={"button-random"}><Link to="/random">Random</Link></li>
-                    <li><Link to="#news">News</Link></li>
-                    <li><Link to="#contact">Contact</Link></li>
+                    <li id={"button-search"}><Link to="/search">Search</Link></li>
                     <li><Link className="active" to="#about">About</Link></li>
                 </ul>
                 <div>
                     <Routes>
+                        <Route path="/" element={<Home/>} />
                         <Route path="/random" element={<RandomCard/>} />
+                        <Route path="/search" element={<RandomCard/>} />
                     </Routes>
                 </div>
             </div>
