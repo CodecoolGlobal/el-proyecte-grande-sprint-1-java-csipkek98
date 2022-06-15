257aa1a109feabf805b868f2338fd8290cc90fda
Péter Markó <peter.marko1089@gmail.com>
2022-06-15 08:00:55 +0200

    Add: SearchCard component and modified CardController

--- src/main/java/com/WizardsOfTheCoast/magic/controller/CardController.java
+++ src/main/java/com/WizardsOfTheCoast/magic/controller/CardController.java
@@ -1,21 +1,16 @@
 package com.WizardsOfTheCoast.magic.controller;
-
-
 import com.WizardsOfTheCoast.magic.model.CardModel;
 import com.WizardsOfTheCoast.magic.service.APIEndpoints;
 import com.WizardsOfTheCoast.magic.service.CardService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@Controller
+@CrossOrigin(origins = {"http://localhost:3000","https://lemon-stone-05afd8203.1.azurestaticapps.net"},
+        methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
+        , allowedHeaders = "*")
+@RestController
 public class CardController {
 
     private CardService cardService;
@@ -25,9 +20,8 @@ public class CardController {
         this.cardService = cardService;
     }
 
-    @GetMapping(value = "/search")
-    @ResponseBody
-    public List<CardModel> getAllCardsTest(@RequestParam(defaultValue="Guest") String name){
+    @GetMapping(value = "/search{name}")
+    public List<CardModel> getAllCardsTest(@PathVariable (required = false) String name){
         List<String> parameters = new ArrayList<>();
         parameters.add(name);
         return cardService.getCards(parameters, APIEndpoints.SEARCH);
