9e16f95fbde8ab3b083216f6652f77fc9be6e11f
csipkek98 <csipkekrisztian@gmail.com>
2022-06-16 00:51:59 +0200

    Add: multiple filter option for search route

--- magictrades-frontend/src/components/SearchCard.js
+++ magictrades-frontend/src/components/SearchCard.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect, useRef} from 'react';
 import axios from 'axios';
 
 const SearchCard = () => {
-    const [inputName, setName] = useState("");
+    const [inputName, setName] = useState(null);
+    const [inputRarity, setRarity] = useState(null);
     const [getCards, setGetCardData] = useState([]);
-    const url = `${process.env.REACT_APP_HOST_URL}/search${inputName}`;
+    // const url = `${process.env.REACT_APP_HOST_URL}/search${inputName}`;
+    let url = `${process.env.REACT_APP_HOST_URL}/card/filter?`;
     const fetchCards = async () => {
         await axios.get(url).then((response) => {
             console.log(response);
@@ -12,17 +14,31 @@ const SearchCard = () => {
             setGetCardData(data);
         });
     }
-    useEffect(() => {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if(inputName != null){
+            url+="name="+inputName
+            if(inputRarity != null){
+                url+="&rarity="+inputRarity
+            }
+        }
         fetchCards().then(r => console.log('i fire once'));
-    },[]);
+    }
+
             return (
                 <div>
+                    <form onSubmit={handleSubmit}>
                     <input type="text" id="message" name="name"
                         value={inputName}
                         onChange={(event) => setName(event.target.value)}
                         autoComplete="off"
                     />
-                    <button id={"button-searchStart"} onClick={fetchCards}>Search Card</button>
+                    <button type={"submit"} id={"button-searchStart"}>Search Card</button><br/>
+                        <label>Rarity type:</label><br/>
+                        <input name={"rarity"} type={"radio"} value={"common"} onChange={(event) => setRarity(event.target.value)}/><label>Common</label><br/>
+                        <input name={"rarity"} type={"radio"} value={"rare"} onChange={(event) => setRarity(event.target.value)}/><label>Rare</label>
+                    </form>
                     <div>
                         {
                             getCards.map(cardObj => (
@@ -36,4 +52,6 @@ const SearchCard = () => {
                 </div>
             );
 };
+
+
 export default SearchCard;
