f2892d00a365a59cdfbaf3b06251c720effd774a
Péter Markó <peter.marko1089@gmail.com>
2022-06-27 12:23:41 +0200

    Add:necessary annotations to the custom card entity

--- src/main/java/com/WizardsOfTheCoast/magic/entity/CustomCardEntity.java
+++ src/main/java/com/WizardsOfTheCoast/magic/entity/CustomCardEntity.java
@@ -1,5 +1,33 @@
 package com.WizardsOfTheCoast.magic.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name ="CustomCard")
 public class CustomCardEntity {
+    @Id
+    @SequenceGenerator(
+            name = "custom_card_sequence",
+            sequenceName = "custom_card_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "custom_card_sequence"
+    )
+    @Column(
+            name ="id",
+            updatable = false,
+            nullable = false
+    )
+    private Long id;
+
 }
