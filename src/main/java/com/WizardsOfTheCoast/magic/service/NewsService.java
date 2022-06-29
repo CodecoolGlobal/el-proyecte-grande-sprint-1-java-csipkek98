bc58531417861628b59579a2d6ed074f1747a82c
Péter Markó <peter.marko1089@gmail.com>
2022-06-29 06:53:52 +0200

    Add: dependency injection and getAllNews method to the NewsService class

--- src/main/java/com/WizardsOfTheCoast/magic/service/NewsService.java
+++ src/main/java/com/WizardsOfTheCoast/magic/service/NewsService.java
@@ -1,8 +1,24 @@
 package com.WizardsOfTheCoast.magic.service;
 
+import com.WizardsOfTheCoast.magic.JPA.NewsRepository;
+import com.WizardsOfTheCoast.magic.entity.NewsEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NewsService {
+
+    private NewsRepository newsRepository;
+
+    @Autowired
+    public NewsService(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    public List<NewsEntity> getAllNews(){
+        return newsRepository.findAll();
+    }
 
 }
