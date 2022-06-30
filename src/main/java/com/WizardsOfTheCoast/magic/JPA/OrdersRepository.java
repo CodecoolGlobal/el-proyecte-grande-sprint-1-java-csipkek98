84207dae2fff41e66261c71fb27f732058a25d64
Péter Markó <peter.marko1089@gmail.com>
2022-06-30 17:56:28 +0200

    Add: OrdersService

--- src/main/java/com/WizardsOfTheCoast/magic/JPA/OrdersRepository.java
+++ src/main/java/com/WizardsOfTheCoast/magic/JPA/OrdersRepository.java
@@ -1,8 +1,9 @@
 package com.WizardsOfTheCoast.magic.JPA;
 
+import com.WizardsOfTheCoast.magic.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrdersRepository extends JpaRepository {
+public interface OrdersRepository extends JpaRepository<Orders, Long> {
 }
