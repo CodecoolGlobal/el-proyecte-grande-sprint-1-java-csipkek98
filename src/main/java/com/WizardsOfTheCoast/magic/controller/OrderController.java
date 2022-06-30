56b0ee65f96455c70c684af1dd2a5784cff2dec1
Péter Markó <peter.marko1089@gmail.com>
2022-06-30 18:05:58 +0200

    Add: DI to the order controller class

--- src/main/java/com/WizardsOfTheCoast/magic/controller/OrderController.java
+++ src/main/java/com/WizardsOfTheCoast/magic/controller/OrderController.java
@@ -1,4 +1,21 @@
 package com.WizardsOfTheCoast.magic.controller;
 
+import com.WizardsOfTheCoast.magic.service.OrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = {"http://localhost:3000","https://lemon-stone-05afd8203.1.azurestaticapps.net"},
+        methods = {RequestMethod.OPTIONS, RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}
+        , allowedHeaders = "*")
+@RestController
 public class OrderController {
+
+    private OrdersService ordersService;
+
+    @Autowired
+    public OrderController(OrdersService ordersService) {
+        this.ordersService = ordersService;
+    }
 }
