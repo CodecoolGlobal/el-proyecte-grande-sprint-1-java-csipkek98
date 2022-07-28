8a8e28d2d42d5f7881c987b79632f7b52db54740
csipkek98 <csipkekrisztian@gmail.com>
2022-07-28 22:09:59 +0200

    Add: jwt token usage for every request in APP.js

--- magictrades-frontend/src/App.js
+++ magictrades-frontend/src/App.js
@@ -1,6 +1,12 @@
-
 import './App.css';
+import {useLocalState} from "./util/useLocalStorage"
+import {useEffect} from "react";
+
 function App() {
+    const [jwt, setJwt] = useLocalState("","jwt")
+
+    // useEffect(() =>{
+    // }, [jwt]);
   return (
         <div>
         </div>
