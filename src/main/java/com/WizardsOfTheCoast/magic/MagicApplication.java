823c8b0729c5e16ed77e3245e3dea031cc3cd33d
csipkek98 <csipkekrisztian@gmail.com>
2022-07-28 22:03:54 +0200

    Add: passwordEncoder for encoding purposes and CommandLineRunner for base data initializing in the database

--- src/main/java/com/WizardsOfTheCoast/magic/MagicApplication.java
+++ src/main/java/com/WizardsOfTheCoast/magic/MagicApplication.java
@@ -1,10 +1,18 @@
 package com.WizardsOfTheCoast.magic;
+import com.WizardsOfTheCoast.magic.entity.*;
+import com.WizardsOfTheCoast.magic.service.UserService;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import javax.sql.DataSource;
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class MagicApplication {
 
@@ -21,6 +29,25 @@ public class MagicApplication {
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(MagicApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userservice){
+		return args -> {
+			userservice.saveRole(new Role(null, "ROLE_USER"));
+			userservice.saveRole(new Role(null, "ROLE_ADMIN"));
+			userservice.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userservice.saveUser(new User(null,"Christian Lace","chris","asd@asd.com","asd125",null,new ArrayList<>(), null, null));
+
+			userservice.addRoleToUser("chris", "ROLE_SUPER_ADMIN");
+			userservice.addRoleToUser("chris", "ROLE_USER");
+		};
 	}
 
 }
