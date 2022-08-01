ca23a090e0ce4aa668e511c8a5a5ebf910a598e3
csipkek98 <csipkekrisztian@gmail.com>
2022-08-01 13:51:45 +0200

    Add: new API routes to SecurityConfig.java

--- src/main/java/com/WizardsOfTheCoast/magic/secutiry/SecurityConfig.java
+++ src/main/java/com/WizardsOfTheCoast/magic/secutiry/SecurityConfig.java
@@ -37,9 +37,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         customAuthenticationFilter.setFilterProcessesUrl("/api/login");
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
-        http.authorizeRequests().antMatchers("/CRUD/api/login/**", "/CRUD/api/token/refresh/**","/randomcard","/search/**").permitAll();
-        http.authorizeRequests().antMatchers(GET,"/api/user/**","/CRUD/custom","/custom","/api/users").hasAnyAuthority("ROLE_USER");
-        http.authorizeRequests().antMatchers(POST,"/api/user/save").hasAnyAuthority("ROLE_ADMIN","ROLE_SUPER_ADMIN");
+        http.authorizeRequests().antMatchers("/CRUD/api/login/**", "/CRUD/api/token/refresh/**","/randomcard","/search/**","/api/usercheck","/api/user/save").permitAll();
+        http.authorizeRequests().antMatchers(GET,"/CRUD/custom","/custom").hasAnyAuthority("ROLE_USER");
+        http.authorizeRequests().antMatchers(POST,"/api/users").hasAnyAuthority("ROLE_ADMIN","ROLE_SUPER_ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
