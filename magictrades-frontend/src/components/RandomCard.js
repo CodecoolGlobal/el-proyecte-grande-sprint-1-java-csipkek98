9bc6823a8e6443cecb3495644d2421b4da2b2b43
csipkek98 <csipkekrisztian@gmail.com>
2022-08-09 16:13:05 +0200

    Modify: RandomCard.js html changes

--- magictrades-frontend/src/components/RandomCard.js
+++ magictrades-frontend/src/components/RandomCard.js
@@ -25,11 +25,9 @@ const RandomCards = () => {
         }
     },[]);
     return (
-
-        // Create new component inside the map
-        // Mapping can be replaced, card component
-
-        <div className="productsContainer" id={"card-details"}>
+        <div
+            // className="productsContainer"
+             id={"card-details"}>
             {Object.keys(cardData).map((key, index) => {
                 if(cardData[key] !== null){
                     if(key === "name"){
@@ -52,7 +50,7 @@ const RandomCards = () => {
                         )}
                     return (
                         <div id={"card-" + key}>
-                            <p className="cardPrice">{cardData[key]}</p>
+                            <p className="cardPrice">Price: {cardData[key]}</p>
                         </div>
                     );
                 }
