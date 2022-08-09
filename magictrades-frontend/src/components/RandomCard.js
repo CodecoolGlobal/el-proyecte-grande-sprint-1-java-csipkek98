c51021e88ebc90357cbdf985055ac4ea569ddb69
csipkek98 <csipkekrisztian@gmail.com>
2022-08-09 11:14:56 +0200

    Modify: Add /api/ route before randomcard url to match backend

--- magictrades-frontend/src/components/RandomCard.js
+++ magictrades-frontend/src/components/RandomCard.js
@@ -2,7 +2,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-const url = `/randomcard`;
+const url = `/api/randomcard`;
 const fetchCards = (abortController) => {
     return axios.get(url, {signal: abortController.signal})
 }
