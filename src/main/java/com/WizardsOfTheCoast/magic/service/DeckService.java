6bba2f6c2b133c7ef3b222ecb9924f88c50e3620
Péter Markó <peter.marko1089@gmail.com>
2022-08-09 11:04:44 +0200

    Add: deleteCardFromDeck method.

--- src/main/java/com/WizardsOfTheCoast/magic/service/DeckService.java
+++ src/main/java/com/WizardsOfTheCoast/magic/service/DeckService.java
@@ -4,9 +4,12 @@ package com.WizardsOfTheCoast.magic.service;
 import com.WizardsOfTheCoast.magic.JPA.CustomCardRepository;
 import com.WizardsOfTheCoast.magic.JPA.DeckRepository;
 import com.WizardsOfTheCoast.magic.JPA.UserRepository;
+import com.WizardsOfTheCoast.magic.entity.CustomCardEntity;
 import com.WizardsOfTheCoast.magic.entity.DeckEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 public class DeckService {
@@ -27,5 +30,16 @@ public class DeckService {
 
     public void saveDeck(DeckEntity deck){
         deckRepository.save(deck);
+    }
+    public void deleteCardFromDeck(DeckEntity deck, Long cardId){
+        CustomCardEntity cardToDelete = null;
+        for (CustomCardEntity card : deck.getCards()) {
+            if(Objects.equals(card.getId(), cardId)){
+                cardToDelete = card;
+            }
+        }
+        deck.removeCard(cardToDelete);
+        assert cardToDelete != null;
+        customCardRepository.delete(cardToDelete);
     }
 }
