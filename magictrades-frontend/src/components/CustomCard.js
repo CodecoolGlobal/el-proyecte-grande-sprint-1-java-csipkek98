0e46c9ff27a73af92adedbcf952aa7304e78ebf8
csipkek98 <csipkekrisztian@gmail.com>
2022-08-09 15:48:01 +0200

    Add: CustomCard authentication with JWT token

--- magictrades-frontend/src/components/CustomCard.js
+++ magictrades-frontend/src/components/CustomCard.js
@@ -2,23 +2,26 @@ import React from 'react';
 import {useEffect, useState, useRef} from "react";
 import axios from "axios";
 import SearchCustomCard from "./SearchCustomCard";
+
 import {Navigate} from "react-router-dom";
 import {wait} from "@testing-library/user-event/dist/utils";
+import ShowAllDecks from "./ShowAllDecks";
 
-// useCallBack, useMemo, useContext
 
 const CustomCard = () => {
     const [inputName, setName] = useState("");
     const [inputPrice, setPrice] = useState("");
     const [inputPic, setPic] = useState("");
     const [getCustomCards, setCustomCardData] = useState([]);
+
     const URI = `/api/users`;
     const [isShown, setIsShown] = useState(false);
     const sessionAttributes = null
     const [isCorrectToken, setIsCorrectToken] = useState(null)
     const isFirstRender = useRef(true);
     const [loading, setLoading] = useState(false);
-    const handleClick = event => {
+
+    const showCards = event => {
         setIsShown(current => !current);
     };
     const fetchCards = () => {
@@ -31,7 +34,12 @@ const CustomCard = () => {
             console.log(response);
             const data = response.data;
             setCustomCardData(data);
+            if (sessionAttributes === null) {
+                setCustomCardData([]);
+            }
+
         });
+
     }
     const postData = (e) => {
         e.preventDefault();
@@ -42,30 +50,29 @@ const CustomCard = () => {
         })
             .then((res) => {
                 setCustomCardData([...getCustomCards, res.data])
-                if (sessionAttributes === null) {
-                    alert("You need to register and login to add a custom card ! ")
-                }
             });
         if (sessionAttributes === null) {
             alert("You need to register and login to add a custom card ! ")
         }
     }
-    const removeUser = async id => {
+    const removeCustomCard = async (id) => {
         try {
-            const res = await axios.delete(`${URI}/${id}`)
-            await fetchCards()
+
+            const url = `api/custom/user/${sessionAttributes}/card_id/${id}`;
+            console.log(id);
+            await axios.delete(url);
+            await fetchCards();
         } catch (error) {
             alert(error)
         }
     }
-
     useEffect(() => {
         if (isFirstRender.current) {
-            if(loading){
+            if (loading) {
                 isAuthenticated()
                 fetchCards()
             }
-        isFirstRender.current = false;
+            isFirstRender.current = false;
         }
     }, [loading])
 
@@ -94,68 +101,80 @@ const CustomCard = () => {
             .then((response) => {
                 setIsCorrectToken(response.status >= 200 && response.status < 300)
             }).catch(() => {
-                alert("authorization failed")
-                setIsCorrectToken(false)
-                localStorage.removeItem("jwt")
+            alert("authorization failed")
+            setIsCorrectToken(false)
+            localStorage.removeItem("jwt")
         })
     }
 
-    if(loading === false){
+    if (loading === false) {
         setLoading(true)
-    }else{
-    if (isCorrectToken === true) {
-        return (
-            <div>
-                <label>Card Name</label>
-                <input className="searchField" type="text" id="inputName" name="name"
-                       value={inputName}
-                       onChange={(event) => setName(event.target.value)}
-                       autoComplete="off"
-                />
-                <label>Price</label>
-                <input className="searchField" type="text" id="inputPrice" name="price"
-                       value={inputPrice}
-                       onChange={(event) => setPrice(event.target.value)}
-                       autoComplete="off"
-                />
-                <label>Pic</label>
-                <input className="searchField" type="text" id="inputPrice" name="pic"
-                       value={inputPic}
-                       onChange={(event) => setPic(event.target.value)}
-                       autoComplete="off"
-                />
-                <button className="searchButton" onClick={postData}>Add custom card !</button>
-                <br/>
-                <br/>
-                <br/>
-                <SearchCustomCard/>
+    } else {
+        if (isCorrectToken === true) {
+            return (
                 <div>
-                    <button className="searchButton" onClick={handleClick}>Show all custom card</button>
-                    {isShown && <div>
-                        {
-                            getCustomCards.map(cardObj => (
-                                    <div key={cardObj.id}>
-                                        <h1>{cardObj.name}</h1>
-                                        <div className="container">
-                                            <p className="cardPrice">Price of the card: {cardObj.price}</p>
-                                            < img src={cardObj.imageUrl} alt="new" className="cardImage"/>
-                                            <button className="searchButton" onClick={() => removeUser(cardObj.id)}> Delete
-                                                this custom card
-                                            </button>
+                    <br/><br/>
+                    <label>Card Name</label>
+                    <input className="searchField" type="text" id="inputName" name="name"
+                           value={inputName}
+                           onChange={(event) => setName(event.target.value)}
+                           autoComplete="off"
+                    /><br/><br/>
+                    <label>Price</label>
+                    <input className="searchField" type="text" id="inputPrice" name="price"
+                           value={inputPrice}
+                           onChange={(event) => setPrice(event.target.value)}
+                           autoComplete="off"
+                    /><br/><br/>
+                    <label>Pic</label>
+                    <input className="searchField" type="text" id="inputPrice" name="pic"
+                           value={inputPic}
+                           onChange={(event) => setPic(event.target.value)}
+                           autoComplete="off"
+                    /><br/><br/>
+                    <button className="searchButton" onClick={postData}>Add custom card !</button>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <SearchCustomCard/>
+                    <div>
+                        <button className="searchButton" onClick={showCards}>Show all custom card</button>
+                        {isShown &&
+                            <div>{
+                                getCustomCards.map(cardObj => (
+                                        <div key={cardObj.id}>
+                                            <h1>{cardObj.name}</h1>
+                                            <div className="container">
+
+                                                <p className="card-price">Price of the card: {cardObj.price}</p>
+                                                <div className={"card-customImageUrl"}>
+                                                    <img src={cardObj.imageUrl} alt="new"/>
+                                                </div>
+                                                <br/>
+                                                <div className={"deleteCardButton"}>
+                                                    <button className="searchButton"
+                                                            onClick={() => removeCustomCard(cardObj.id)}> Delete this
+                                                        custom card
+                                                    </button>
+                                                </div>
+
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                            )}
-                    </div>}
+                                    )
+                                )}
+                            </div>}
+                        <div>
+                            <ShowAllDecks/>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        );
-    } else if(isCorrectToken === false) {
-        return <Navigate to={"/login"}/>
-    }else{
-        wait(2000)
+            );
+        } else if (isCorrectToken === false) {
+            return <Navigate to={"/login"}/>
+        } else {
+            wait(2000)
+        }
     }
-    }
-};
+}
 
 export default CustomCard;
