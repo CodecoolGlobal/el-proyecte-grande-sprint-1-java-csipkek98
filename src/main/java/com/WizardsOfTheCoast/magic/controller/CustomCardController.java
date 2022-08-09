f00a81fa7a3f02312f9ba3e25b19dd2e36796b16
csipkek98 <csipkekrisztian@gmail.com>
2022-08-09 16:08:21 +0200

    Modify: CustomCardController.java

--- src/main/java/com/WizardsOfTheCoast/magic/controller/CustomCardController.java
+++ src/main/java/com/WizardsOfTheCoast/magic/controller/CustomCardController.java
@@ -1,15 +1,18 @@
 package com.WizardsOfTheCoast.magic.controller;
 
-import com.WizardsOfTheCoast.magic.Data_sample.CustomCardCreator;
-import com.WizardsOfTheCoast.magic.entity.CollectionEntity;
 import com.WizardsOfTheCoast.magic.entity.CustomCardEntity;
-import com.WizardsOfTheCoast.magic.service.CollectionService;
-import com.WizardsOfTheCoast.magic.service.customCardService;
+import com.WizardsOfTheCoast.magic.entity.DeckEntity;
+import com.WizardsOfTheCoast.magic.entity.User;
+import com.WizardsOfTheCoast.magic.service.DeckService;
+import com.WizardsOfTheCoast.magic.service.UserService;
+import com.WizardsOfTheCoast.magic.service.CustomCardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
-
-import java.util.ArrayList;
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 
@@ -21,73 +24,67 @@ import java.util.Map;
         , allowedHeaders = "*")
 @RestController("/api")
 public class CustomCardController {
-    private customCardService cardService;
-    private CollectionService collectionService;
-
-    public CustomCardController(CollectionService collectionService) {
-        this.collectionService = collectionService;
-    }
+    private CustomCardService cardService;
+    private UserService userService;
+    private DeckService deckService;
 
     @Autowired
-    public void setService(customCardService cardService) {
+    public CustomCardController(CustomCardService cardService, UserService userService, DeckService deckService) {
         this.cardService = cardService;
+        this.userService = userService;
+        this.deckService = deckService;
     }
+
 
     @GetMapping(value = "/")
     public List<CustomCardEntity> Greetings(){
-//        initCards();
-//        for(CustomCardEntity card: cardService.getAllCustomCard()){
-//            System.out.println(card.getName() + "init");
-//        }
+
         return cardService.getAllCustomCard();
     }
 
-    @GetMapping(value = "/custom")
-    public List<CustomCardEntity> getClients() {
-
-        return cardService.getAllCustomCard();
+    @GetMapping(value = "/custom/{id}")
+    public List<CustomCardEntity> getClients(@PathVariable String id) {
+        System.out.println(id);
+        User user = userService.findUserById(Long.valueOf(id));
+        return userService.getAllUserCards(Long.valueOf(id));
     }
 
     @PostMapping(value = "/custom")
-    public CustomCardEntity addCustomCard(@RequestBody Map<String, Object> payLoad) {
-        String username = (String)payLoad.get("username");
-        CollectionEntity collection = collectionService.getCollectionByName(username);
+    public ResponseEntity<CustomCardEntity> addCustomCard(
+            @RequestBody @Valid Map<String, Object> payLoad, UriComponentsBuilder uriComponentsBuilder) {
         CustomCardEntity customCard = CustomCardEntity.builder()
                 .name((String)payLoad.get("name"))
                 .imageUrl((String)payLoad.get("pic"))
                 .price(Integer.parseInt((String)payLoad.get("price")))
-                .collection(collection)
-        .build();
-        customCard.setCollection(collection);
-        cardService.addCard(customCard);
-        return customCard;
-    }
-
-    @PostMapping(value = "/custom/create")
-    public CollectionEntity addCollection(){
-        List<CustomCardEntity> empty = new ArrayList<>();
-        CollectionEntity collection = CollectionEntity.builder()
-                .cards(empty)
                 .build();
-        collectionService.saveCollection(collection);
-        return collection;
+        long l=Long.parseLong((String) payLoad.get("sessionId"));
+        User user = userService.findUserById(l);
+        user.addCard(customCard);
+        customCard.setUser(user);
+        cardService.saveCard(customCard);
+        URI location = uriComponentsBuilder.path("/custom/search/{id}/{name}")
+                .buildAndExpand(user.getId(), customCard.getName()).toUri();
+        return ResponseEntity.created(location).body(customCard);
     }
 
-    @DeleteMapping(value = "/custom/{id}")
-    public void deleteCustomCard(@PathVariable String id){
-        cardService.deleteCustomCardById(Long.parseLong(id));
-    }
 
-    @GetMapping(value = "/custom/{username}/{searchName}")
-    public CustomCardEntity findCustomCardByName(@PathVariable String username, @PathVariable String searchName){
-        CollectionEntity collection = collectionService.getCollectionByName(username);
-        return cardService.findCustomCardFromCollectionByName(collection.getCards(), searchName);
-    }
-
-    public void initCards(){
-        List<CustomCardEntity> initCards = CustomCardCreator.initialize();
-        for (CustomCardEntity initCard : initCards) {
-            cardService.addCard(initCard);
+    @DeleteMapping(value = "/custom/user/{user_id}/card_id/{id}")
+    public void deleteCustomCard(@PathVariable String id, @PathVariable String user_id){
+        CustomCardEntity cardToDelete = cardService.findCardById(Long.valueOf(id));
+        DeckEntity deck = userService.checkIfCardInAnyDeck(Long.valueOf(user_id), cardToDelete);
+        if(deck != null){
+            deckService.deleteCardFromDeck(deck, Long.valueOf(id));
+        }
+        else{
+            cardService.deleteCustomCardById(Long.parseLong(id));
         }
     }
+
+
+    @GetMapping(value = "/custom/search/{id}/{name}")
+    public ResponseEntity<CustomCardEntity>  findCustomCardByName(@PathVariable String id, @PathVariable String name){
+        User user = userService.findUserById(Long.valueOf(id));
+        return ResponseEntity.ok().body(user.findCustomCardByName(name));
+    }
+
 }
