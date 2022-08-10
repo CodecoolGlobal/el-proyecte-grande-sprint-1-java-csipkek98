78c536eea9dfdd8e2d3fb4faa2b65a9e2adea83d
Péter Markó <peter.marko1089@gmail.com>
2022-08-10 17:38:41 +0200

    Modify: Pagination component .

--- magictrades-frontend/src/components/Pagination.js
+++ magictrades-frontend/src/components/Pagination.js
@@ -6,28 +6,62 @@ const Pagination = ({ data, RenderComponent, title, pageLimit, dataLimit }) => {
     const [currentPage, setCurrentPage] = useState(1);
 
     function goToNextPage() {
-        // not yet implemented
+        setCurrentPage((page) => page + 1);
     }
 
     function goToPreviousPage() {
-        // not yet implemented
+        setCurrentPage((page) => page - 1);
     }
 
     function changePage(event) {
-        // not yet implemented
+        const pageNumber = Number(event.target.textContent);
+        setCurrentPage(pageNumber);
     }
 
     const getPaginatedData = () => {
-        // not yet implemented
+        const startIndex = currentPage * dataLimit - dataLimit;
+        const endIndex = startIndex + dataLimit;
+        return data.slice(startIndex, endIndex);
     };
 
     const getPaginationGroup = () => {
-        // not yet implemented
+        let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+        return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
     };
 
     return (
         <div>
-            
+            <h1>{title}</h1>
+            <div className="pagination">
+                <button
+                    onClick={goToPreviousPage}
+                    disabled={currentPage === 1}
+                    className={`searchButton ${currentPage === 1 ? 'disabled' : ''}`}
+                >
+                    prev
+                </button>
+                {getPaginationGroup().map((item, index) => (
+                    <button
+                        key={index}
+                        onClick={changePage}
+                        className={`searchButton ${currentPage === item ? 'active' : null}`}
+                    >
+                        <span>{item}</span>
+                    </button>
+                ))}
+                <button
+                    onClick={goToNextPage}
+                    disabled={currentPage === pages}
+                    className={`searchButton ${currentPage === pages ? 'disabled' : ''}`}
+                >
+                    next
+                </button>
+                <div className="dataContainer">
+                    {getPaginatedData().map((d, idx) => (
+                        <RenderComponent key={idx} data={d} />
+                    ))}
+                </div>
+            </div>
         </div>
     );
 };
