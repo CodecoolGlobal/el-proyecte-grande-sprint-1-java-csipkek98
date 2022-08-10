aa5c6a96c1b784fcd40c82d89ad8666d008aca8e
Péter Markó <peter.marko1089@gmail.com>
2022-08-10 15:51:47 +0200

    Add: Pagination component with boilerplate functions.

---
+++ magictrades-frontend/src/components/Pagination.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+
+const Pagination = ({ data, RenderComponent, title, pageLimit, dataLimit }) => {
+
+    const [pages] = useState(Math.round(data.length / dataLimit));
+    const [currentPage, setCurrentPage] = useState(1);
+
+    function goToNextPage() {
+        // not yet implemented
+    }
+
+    function goToPreviousPage() {
+        // not yet implemented
+    }
+
+    function changePage(event) {
+        // not yet implemented
+    }
+
+    const getPaginatedData = () => {
+        // not yet implemented
+    };
+
+    const getPaginationGroup = () => {
+        // not yet implemented
+    };
+
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Pagination;
